adf5d83caa9cd956af61ad10a2b3b6b5d5b28e15
simplyrequired <54539016+simplyrequired@users.noreply.github.com>
2019-09-06 20:11:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
-
 const YTDL = require('ytdl-core');
 
 function Play(connection, message)
