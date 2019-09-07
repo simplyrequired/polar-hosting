2accc9c5ec0b39db670a96b58b9cf27672eed2e9
simplyrequired <54539016+simplyrequired@users.noreply.github.com>
2019-09-07 21:09:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -102,13 +102,14 @@ if(message.author.bot) return;
     if(!message.member.roles.some(r=>["[C] Commandant", "[D] Developer"].includes(r.name)) )
     return message.reply("Sorry! you don't have permissions to use this!");
     const m = await message.channel.send("Getting Discord API working..");
-     const embed = new RichEmbed()
     m.channel.send('Database has found 1 file.')
     m.channel.send('Sent file to your dm.')
+     const embed = new RichEmbed(){
         .setTitle('Lastest Update to script')
         .setColor(0xFFFFFF)
         .setDescription(`Hello ww2! This is the last updated script that Simply made for you.\nhttps://www.roblox.com/library/3826714455/A-webhook`);
       m.author.send(embed);
+    }
     }
 });
 
