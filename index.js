757e6c84820a01d32e4a5f01d7581024dd825e84
simplyrequired <54539016+simplyrequired@users.noreply.github.com>
2019-09-07 22:22:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ client.on('message', message => {
     .setFooter('Special Operations Bot',);
   message.channel.send(embed);
   }
-  if (message.content === '/join') {
+  if (message.content === ';join') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
@@ -142,5 +142,6 @@ client.on('message', message => {
     } else {
       message.reply('You need to join a voice channel first!');
     }
+  }
 });
 client.login(process.env.BOT_TOKEN);
