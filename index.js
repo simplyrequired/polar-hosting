b1af266fa58de22a634da99fcd70131f97c71f89
simplyrequired <54539016+simplyrequired@users.noreply.github.com>
2019-09-18 19:15:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,6 @@ if(command === "hr-announce") {
   }
   
    if(command === "messagefromsimply") {
-    return message.reply("Ok...");
     message.channel.send("This message is send from Simply_Required, I know the reason that I cant join the server...Me and com are fixing this\nDon't blame BEST FOR THIS. It's my own fault and now leave it ok bye cya later nubs.");
   }
   
