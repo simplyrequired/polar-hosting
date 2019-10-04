67c12e1c05d26914ef62d7e9669979964d1a8ab5
simplyrequired <54539016+simplyrequired@users.noreply.github.com>
2019-10-04 22:25:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,190 +1,181 @@
-const Discord = require('discord.js');
+// - Discord Packages - \\
+const Discord = require("discord.js");
+const roblox = require("noblox.js");
 const Client = new Discord.Client();
-const Prefix = ';';
 
-// - Role Config - \\
-const CreatorRole = "Polar Bot";
-const HRRole = "SHR";
-const DevRole = "SHR";
-const Creator = 'Simply_Required';
-const ServerName = 'Polar Airlines'
-
-Client.on('ready', ()=> {
-    console.log(`Logged in with username: ${Client.user.username}`)
-    
+// - Bot settings - \\
+const Prefix = ";";
 
 
-Client.on('guildMemberAdd', member => {
-        const channel = member.guild.channels.find(ch => ch.name === 'general');
-        if (!channel) return;
-        channel.send(`Welcome to the server, ${member}`);
-      });
-
-Client.on('guildMemberRemove', member => {
-        const channel = member.guild.channels.find(ch => ch.name === 'general');
-        if (!channel) return;
-        channel.send(`${member} has left 😔`);
-      });
-})
-
-
-
-
-
-
-// - Fun Commands - \\ 
-Client.on("message", async message => {
-    if(message.author.bot) return;
-      if(message.content.indexOf(Prefix) !== 0) return;
-      const args = message.content.slice(Prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
-
-      if(command === "commandant") {
-            const m = await message.channel.send("Ping?");
-            m.edit(`hes very hot xd`)
-    }
-
-    
- // - Creator Command - \\
- if(command === "creator") {
-    const m = await message.channel.send(`My creator/owner is: ${Creator}.`);
-  }
-  
-  
-  
-   // - Help Command - \\
-   if(command === "help") {
-    const m = await message.channel.send("Need help?");
-    m.edit(`Nothing much yet!`)
-  }
- 
-  
-    // - Ping Command - \\
-    if(command === "ping") {
-      const m = await message.channel.send("Ping?");
-      m.edit(`>>> **Pong!**\n⏱${m.createdTimestamp - message.createdTimestamp}ms\n❤️ ${Math.round(Client.ping)}ms`);
-    }
+// - Welcome Settings - \\
+Client.on("guildMemberAdd", member => {
+  const channel = member.guild.channels.find(ch => ch.name === "welcome");
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+    .setTitle(`User joined!`)
+    .setColor(0xffffff)
+    .setDescription(`Hey ${member}! Welcome to Juiceyz`)
+    .setFooter("SecurityService")
+    .setTimestamp();
+  channel.send(embed);
 });
 
+Client.on("guildMemberRemove", member => {
+  const channel = member.guild.channels.find(ch => ch.name === "welcome");
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+    .setTitle(`User left...`)
+    .setColor(0xffffff)
+    .setDescription(`Aww! ${member} come back soon!`)
+    .setFooter("SecurityService")
+    .setTimestamp();
+  channel.send(embed);
+});
 
+// - Commando Settings - \\
+Client.on("ready", () => {
+  console.log(
+    `Bot has started, with ${Client.users.size} users, in ${Client.channels.size} channels of ${Client.guilds.size} guilds.`
+  );
+  console.log(`Logged in as: ${Client.user.username} BotID: ${Client.user.id}`);
+  Client.user.setPresence({
+    game: {
+      name: "SecurityServiceV1",
+      type: "STREAMING",
+      url: "https://www.twitch.tv/simplyrequiredrblx"
+    }
+  });
+});
 
-
-// - Permissions needed+ Commands - \\
 Client.on("message", async message => {
-  if(message.author.bot) return;
-    if(message.content.indexOf(Prefix) !== 0) return;
-    const args = message.content.slice(Prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+  if (message.author.bot) return;
+  if (message.content.indexOf(Prefix) !== 0) return;
+  const args = message.content
+    .slice(Prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-  // - Annouce Command - \\
-  if(command === "announce") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.channel.send(`Hey! ${message.author}, This command is only for HR+ only. Are you HR+?\nAsk to update your roles.`);
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const channel = message.guild.channels.find(ch => ch.name === 'announcements');
-  if (!channel) message.channel.send('I could not find the channel!');
-         channel.send(sayMessage);
-          message.channel.send('Succesfully announced!')
-  
+  if (command === "ping") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Ping?")
+      .setColor(0xffffff)
+      .setDescription(`❤️ ${Math.round(Client.ping)}ms`)
+      .setFooter("SecurityService")
+      .setTimestamp();
+    message.channel.send(embed);
   }
-      if(command === "flight-announce") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.channel.send(`Hey! ${message.author}, This command is only for HR+ only. Are you HR+?\nAsk to update your roles.`);
+
+  if (command === "development") {
+    if (!message.member.roles.some(r => [`BotAuth`].includes(r.name)))
+      return message.channel.send(
+        `Hey! ${message.author}, You don't have the permissions to use this command!`
+      );
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const channel = message.guild.channels.find(ch => ch.name === 'flight-announcements');
-  if (!channel) message.channel.send('I could not find the channel!');
-         channel.send(sayMessage);
-              message.channel.send('Succesfully announced!')
-  
+    message.delete().catch(O_o => {});
+    const channel = message.guild.channels.find(
+      ch => ch.name === "development-announcements"
+    );
+    if (!channel) message.channel.send("I could not find the channel!");
+    const embed = new Discord.RichEmbed()
+      .setTitle("Development Announcement")
+      .setColor(0xffffff)
+      .setDescription(sayMessage)
+      .setFooter("SecurityService")
+      .setTimestamp();
+    channel.send(embed);
+    message.channel.send("Alright! Sended");
   }
-      if(command === "dev-announce") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.channel.send(`Hey! ${message.author}, This command is only for HR+ only. Are you HR+?\nAsk to update your roles.`);
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const channel = message.guild.channels.find(ch => ch.name === 'development-announcements');
-  if (!channel) message.channel.send('I could not find the channel!');
-         channel.send(sayMessage);
-              message.channel.send('Succesfully announced!')
   
-  }
-          if(command === "event") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.channel.send(`Hey! ${message.author}, This command is only for HR+ only. Are you HR+?\nAsk to update your roles.`);
+    if (command === "staff-announce") {
+    if (!message.member.roles.some(r => [`BotAuth`].includes(r.name)))
+      return message.channel.send(
+        `Hey! ${message.author}, You don't have the permissions to use this command!`
+      );
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const channel = message.guild.channels.find(ch => ch.name === 'events');
-  if (!channel) message.channel.send('I could not find the channel!');
-         channel.send(sayMessage);
-                  message.channel.send('Succesfully announced!')
+    message.delete().catch(O_o => {});
+    const channel = message.guild.channels.find(
+      ch => ch.name === "staff-announcements"
+    );
+    if (!channel) message.channel.send("I could not find the channel!");
+    const embed = new Discord.RichEmbed()
+      .setTitle("Staff Announcement")
+      .setColor(0xffffff)
+      .setDescription(sayMessage)
+      .setFooter("SecurityService")
+      .setTimestamp();
+    channel.send(embed);
+    message.channel.send("Alright! Sended");
+  }
+
+  if (command === "suggest") {
+    if (!message.member.roles.some(r => [`Verified`].includes(r.name)))
+      return message.channel.send(
+        `Hey! ${message.author}, You don't have the permissions to use this command!`
+      );
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o => {});
+    const channel = message.guild.channels.find(
+      ch => ch.name === "suggestions"
+    );
+    if (!channel) message.channel.send("I could not find the channel!");
+    const embed = new Discord.RichEmbed()
+      .setTitle(`${message.author.username}'s Suggestion`)
+      .setColor(0xffffff)
+      .setThumbnail(`${message.author.displayAvatarURL}`)
+      .setDescription(sayMessage)
+      .setFooter("SecurityService")
+      .setTimestamp();
+    channel.send(embed);
+  }
+  if (command === "products") {
+    if (!message.member.roles.some(r => [`Verified`].includes(r.name)))
+      return message.channel.send(
+        `Hey! ${message.author}, You don't have the permissions to use this command!`
+      );
+    const embed = new Discord.RichEmbed()
+      .setTitle(`${message.author.username}'s Purchase list`)
+      .setColor(0xffffff)
+      .setThumbnail(`${message.author.displayAvatarURL}`)
+      .setDescription("**None**")
+      .setFooter("SecurityService")
+      .setTimestamp();
+    message.channel.send(embed);
+  }
   
-  }
-              if(command === "staff-announce") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.channel.send(`Hey! ${message.author}, This command is only for HR+ only. Are you HR+?\nAsk to update your roles.`);
+  if (command === "say") {
+    if (!message.member.roles.some(r => [`BotAuth`].includes(r.name)))
+      return message.reply(
+        "Sorry! You don't have any permissions to use this command."
+      );
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const channel = message.guild.channels.find(ch => ch.name === 'staff-announcements');
-  if (!channel) message.channel.send('I could not find the channel!');
-         channel.send(sayMessage);
-                      message.channel.send('Succesfully announced!')
-  
-  }
-  // - An Usefull trollling command- \\
-  if(command === "say") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${DevRole}`].includes(r.name)) )
-    return message.reply("Ok...");
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
+    message.delete().catch(O_o => {});
     message.channel.send(sayMessage);
   }
-  if(command === "talk") {
-    if(!message.member.roles.some(r=>[`${CreatorRole}`, `${HRRole}`].includes(r.name)) )
-    return message.reply("Ok...");
+  if (command === "talk") {
+    if (!message.member.roles.some(r => [`BotAuth`].includes(r.name)))
+      return message.reply(
+        "Sorry! You don't have any permissions to use this command."
+      );
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
+    message.delete().catch(O_o => {});
     message.channel.send(sayMessage);
   }
 
+  if (command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if (!message.member.roles.some(r => [`BotAuth`].includes(r.name)))
+      if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.reply(
+          "Please provide a number between 2 and 100 for the number of messages to delete"
+        );
+
+    const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+    message.channel
+      .bulkDelete(fetched)
+      .catch(error => message.reply(`${error}`));
+  }
 });
 
-// - Adminstrative Commands - \\
-Client.on("message", async message => {
-  if(message.author.bot) return;
-    if(message.content.indexOf(Prefix) !== 0) return;
-    const args = message.content.slice(Prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
 
-  // - Ban User - \\
-  if(command === "kick") {
-message.channel.send("This command is perm disabled due issues");
-
-  }
-  
-  if(command === "ban") {
-message.channel.send("This command is perm disabled due issues");
-  }
-  // - Purge message - \\
-if(command === "purge") {
-  const deleteCount = parseInt(args[0], 10);
-
-  if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-    return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-  
-  const fetched = await message.channel.fetchMessages({limit: deleteCount});
-  message.channel.bulkDelete(fetched)
-    .catch(error => message.reply(`${error}`));
-}
-
-});
-
-// - Creator & Server owner Only Private Commands - \\
-
-
-
-
-
-// - Client Login - \\
-Client.login(process.env.BOT_TOKEN);
+Client.login(process.env.TOKEN);
